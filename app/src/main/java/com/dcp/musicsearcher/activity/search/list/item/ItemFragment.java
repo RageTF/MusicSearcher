cc2df3d3ce648873f65a5ab376be85db6a3742d2
RageTF <m_200_6@mail.ru>
2017-01-16 12:09:26 +0300

    release.v2

--- app/src/main/java/com/dcp/musicsearcher/activity/search/list/item/ItemFragment.java
+++ app/src/main/java/com/dcp/musicsearcher/activity/search/list/item/ItemFragment.java
@@ -6,10 +6,12 @@ import android.support.v4.app.Fragment;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.dcp.musicsearcher.api.ApiRequester;
 import com.dcp.musicsearcher.api.pojo.lyrics.LyricSearch;
 import com.dcp.musicsearcher.api.pojo.lyrics.Message;
+import com.google.gson.JsonSyntaxException;
 
 import java.io.IOException;
 
@@ -25,18 +27,19 @@ public class ItemFragment extends Fragment {
     private MyAsyncTask mMyAsyncTask;
     private long mItemId;
 
-    public static ItemFragment newInstance(long itemId){
-        ItemFragment itemFragment=new ItemFragment();
-        Bundle bundle=new Bundle();
+    public static ItemFragment newInstance(long itemId) {
+        ItemFragment itemFragment = new ItemFragment();
+        Bundle bundle = new Bundle();
         bundle.putLong("itemId", itemId);
+        Log.v("IDDDD", String.valueOf(itemId));
         itemFragment.setArguments(bundle);
         return itemFragment;
     }
 
-    public void start(){
-        if(mMyAsyncTask==null){
-            mItemId=getArguments().getLong("itemId");
-            mMyAsyncTask=new MyAsyncTask(mItemId);
+    public void start() {
+        if (mMyAsyncTask == null) {
+            mItemId = getArguments().getLong("itemId");
+            mMyAsyncTask = new MyAsyncTask(mItemId);
             mMyAsyncTask.execute();
         }
     }
@@ -44,21 +47,21 @@ public class ItemFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
-        mTaskInterface=null;
+        mTaskInterface = null;
     }
 
     @Override
     public void onAttach(Context context) {
-        if(context instanceof TaskInterface){
-            mTaskInterface= (TaskInterface) context;
+        if (context instanceof TaskInterface) {
+            mTaskInterface = (TaskInterface) context;
         }
         super.onAttach(context);
     }
 
     @Override
     public void onAttach(Activity activity) {
-        if(activity instanceof TaskInterface){
-            mTaskInterface= (TaskInterface) activity;
+        if (activity instanceof TaskInterface) {
+            mTaskInterface = (TaskInterface) activity;
         }
         super.onAttach(activity);
     }
@@ -67,47 +70,49 @@ public class ItemFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
-        mItemId=getArguments().getLong("itemId");
+        mItemId = getArguments().getLong("itemId");
         MyAsyncTask myAsyncTask = new MyAsyncTask(mItemId);
         myAsyncTask.execute();
     }
 
-    private class MyAsyncTask extends AsyncTask<Void, Integer, String>{
+    private class MyAsyncTask extends AsyncTask<Void, Integer, String> {
 
         private long mItemId;
 
-        MyAsyncTask(long ItemId){
-            this.mItemId=ItemId;
+        MyAsyncTask(long ItemId) {
+            this.mItemId = ItemId;
         }
 
         @Override
         protected void onPreExecute() {
-            if(mTaskInterface!=null){
+            if (mTaskInterface != null) {
                 mTaskInterface.onTaskStart();
             }
         }
 
         @Override
         protected void onPostExecute(String s) {
-            mMyAsyncTask=null;
-            if(mTaskInterface!=null){
-                if(s.equals("Error")){
+            mMyAsyncTask = null;
+            if (mTaskInterface != null) {
+                if (s.equals("Error")) {
                     mTaskInterface.onError();
-                }else if(s.equals("")){
+                } else if (s.equals("")) {
                     mTaskInterface.onFinish("Sorry, text not found");
-                }else mTaskInterface.onFinish(s);
+                } else mTaskInterface.onFinish(s);
             }
         }
 
         @Override
         protected String doInBackground(Void... params) {
-            ApiRequester apiRequester=ApiRequester.getInstance();
-            String lyrics="";
+            ApiRequester apiRequester = ApiRequester.getInstance();
+            String lyrics = "";
             try {
-                Response<LyricSearch> response=apiRequester.getResultLyricsSearch(mItemId);
-                lyrics=response.body().getMessage().getBody().getLyrics().getLyricsBody();
+                Response<LyricSearch> response = apiRequester.getResultLyricsSearch(mItemId);
+                lyrics = response.body().getMessage().getBody().getLyrics().getLyricsBody();
+            }catch (JsonSyntaxException e){
+                return "";
             } catch (IOException e) {
-                lyrics="Error";
+                lyrics = "Error";
             }
             return lyrics;
         }
